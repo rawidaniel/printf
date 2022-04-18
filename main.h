f68ad80decf1596f3772d0cc4275f6060d98c62f
alemenew <alemenewendawek@gmail.com>
2022-04-18 21:29:30 +0300

    add function

--- main.h
+++ main.h
@@ -1,18 +1,24 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-
-
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
+/* format checker*/
 int print_format(const char *format, va_list valist);
 int formatchecker(const char *str, va_list valist, int *j);
+/*print the output to stdout*/
 int print_out(char c);
+/*print anything*/
 int _printf(const char *format, ...);
+/*print a character and string*/
 int print_char(va_list valist);
 int print_string(va_list valist);
+/* print integer */
+int print_int(va_list valist);
+void print_number(int n);
+int number_count(int n);
 
 /**
  * struct format - a struct of format
@@ -25,5 +31,7 @@ typedef struct format
 	char l;
 	int (*ptr)(va_list);
 } Data;
+
+
 
 #endif
