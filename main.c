bc4e6461235895dc5ba18be27746ec5281123cce
alemenew <alemenewendawek@gmail.com>
2022-04-18 14:31:35 +0300

    fix

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+{
+
+	 _printf("Let's try to printf a simple sentence.\n");
+	 printf("Let's try to printf a simple sentence.\n");
+
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+
+	return (0);
+}
